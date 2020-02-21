b26a43c23793ecd34694731fd5a4c94d922b852b
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-21 01:16:47 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,11 +4,9 @@
 
 # end
 
-all_odd = [1,3].all? do |number|
-  number.odd? # Will evaluate to true for 1, true for 3
-end #=> true
-all_odd #=> true
-
-[4, 8, 9, 984].any?{|i| i < 9842}
-
-[1,2,3].detect{|i| i.odd?} #=> 1
+def my_all?(collection)
+  i = 0
+  while i < collection.length
+    i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
+  end
+end
